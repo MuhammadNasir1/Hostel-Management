a892dba6dd8ac486f762867fae55870297322bae
M-Arham Waheed <info.epgae.org@gmail.com>
2024-04-24 15:36:55 +0500

    Initial Commit

--- course.php
+++ course.php
@@ -6,6 +6,38 @@ if (@!$_SESSION['login']) {
 
 $title = "Couses";
 include("./includes/header.php");
+
+
+if (isset($_POST['submit'])) {
+    $course_name = $_POST['course_name'];
+    $no_of_year = $_POST['no_of_year'];
+    $course_status = $_POST['course_status'];
+
+    $insert = "INSERT INTO `course`(`course_name`, `no_of_year`, `course_status`) VALUES ('$course_name','$no_of_year','$course_status')";
+    $query = mysqli_query($conn, $insert);
+
+    if ($insert) {
+        header('location: course.php');
+    } else {
+        echo "Data Not Added";
+    }
+}
+
+$get_data = "SELECT * FROM `course`";
+$result = mysqli_query($conn, $get_data);
+
+if (isset($_REQUEST['del'])) {
+    $id = $_REQUEST['del'];
+    $delete = "DELETE FROM `course` WHERE id = $id";
+    $query = mysqli_query($conn, $delete);
+
+    if ($query) {
+        header('location: course.php');
+    } else {
+        echo 'Data Not Added';
+    }
+}
+
 ?>
 
 <div class="container-fluid mt-4">
@@ -13,38 +45,44 @@ include("./includes/header.php");
     <div class=" shadow p-4 rounded-1">
         <div class="d-flex justify-content-between  mb-5">
             <h5 class="text-primary ">Students List</h5>
-            <button class="btn bg-primary text-white px-3 py-2 fw-semibold" data-bs-toggle="modal" data-bs-target="#exampleModal">Add Student</button>
+            <button class="btn bg-primary text-white px-3 py-2 fw-semibold" data-bs-toggle="modal" data-bs-target="#exampleModal">Add Course</button>
         </div>
         <table class="table table-responsive" id="myTable">
             <thead>
                 <tr>
                     <th>Sno.</th>
-                    <th>Corse Name</th>
-                    <th></th>
+                    <th>Course Name</th>
+                    <th>Year</th>
+                    <th>Course Status</th>
+                    <th>Action</th>
 
                 </tr>
             </thead>
             <tbody>
-                <tr>
-                    <td>01</td>
-                    <td>peter</td>
-                    <td>peter@email.com</td>
+                <?php
+                $a = 0;
+                while ($row = mysqli_fetch_assoc($result)) {
+                    $a++;
+                ?>
+                    <tr>
+                        <td><?= $a ?></td>
+                        <td><?= $row['course_name'] ?></td>
+                        <td><?= $row['no_of_year'] ?></td>
+                        <td><?= $row['course_status'] ?></td>
+                        <td>
+                            <a href="course.php?edit=<?= $row['id'] ?>"><button class="btn bg-primary text-white btn-sm">Edit</button></a>
+                            <a href="course.php?del=<?= $row['id'] ?>"><button class="btn bg-primary text-white btn-sm">Delete</button></a>
+                        </td>
 
-                </tr>
+                    </tr>
+                <?php
+                }
+                ?>
             </tbody>
         </table>
     </div>
 </div>
-<div class="container-fluid">
-    <div class="row">
-        <div class="col-xl-3 col-lg-3 col-md-4 col-sm-6 ">
-            <div class="mb-3">
-                <label for="studentName" class="formlabel mt-1 fs-6 text-primary">Student Name</label>
-                <input type="text" class="form-control mt-1">
-            </div>
-        </div>
-    </div>
-</div>
+
 
 
 <!-- Modal -->
@@ -57,18 +95,30 @@ include("./includes/header.php");
             </div>
             <div class="modal-body">
                 <div class="container-fluid">
-                    <div class="row">
-                        <div class="col-xl-3 col-lg-4 col-md-4 col-sm-6 ">
-                            <div class="mb-3">
-                                <label for="studentName" class="formlabel mt-1 fs-6 text-primary">Student Name</label>
-                                <input type="text" class="form-control mt-1">
+                    <form action="#" method="post" class="form">
+                        <div class="row">
+                            <div class="col-md-4 col-12">
+                                <label for="course_name" class="form-label text-primary">Course Name</label>
+                                <input type="text" name="course_name" placeholder="Enter Course Name" id="course_name" class="form-control">
+                            </div>
+                            <div class="col-md-4 col-12">
+                                <label for="no_of_year" class="form-label">No Of Year</label>
+                                <input type="number" min="1" name="no_of_year" id="no_of_year" placeholder="No Of Year" class="form-control">
+                            </div>
+                            <div class="col-md-4 col-12">
+                                <label for="course_status" class="form-label">Status</label>
+                                <select name="course_status" id="course_status" class="form-select">
+                                    <option value="Active">Active</option>
+                                    <option value="InActive">InActive</option>
+                                </select>
                             </div>
                         </div>
-                    </div>
+
                 </div>
             </div>
             <div class="modal-footer">
-                <button class="btn bg-primary text-white px-3 py-2 fw-semibold">Save</button>
+                <button type="submit" name="submit" class="btn bg-primary text-white px-3 py-2 fw-semibold">Save</button>
+                </form>
             </div>
         </div>
     </div>
--- student.php
+++ student.php
@@ -6,6 +6,48 @@ if (@!$_SESSION['login']) {
 
 $title = "STUDENT";
 include("./includes/header.php");
+
+
+if (isset($_POST['save'])) {
+    $name = $_POST['name'];
+    $roll_no = $_POST['roll_no'];
+    $father_name = $_POST['father_name'];
+    $mother_name = $_POST['mother_name'];
+    $contact_no = $_POST['contact_no'];
+    $date_of_birth = $_POST['date_of_birth'];
+    $parent_no = $_POST['parent_no'];
+    $course = $_POST['course'];
+    $gender = $_POST['gender'];
+    $blood_group = $_POST['blood_group'];
+    $status = $_POST['status'];
+    $address = $_POST['address'];
+
+    $insert = "INSERT INTO `students`(`name`, `roll_no`, `father_name`, `mother_name`, `contact_no`, `date_of_birth`, `parent_no`, `course`, `gender`, `blood_group`, `status`, `address`) VALUES ('$name','$roll_no','$father_name','$mother_name','$contact_no','$date_of_birth','$parent_no','$course','$gender','$blood_group','$status','$address')";
+    $query = mysqli_query($conn, $insert);
+
+    if ($query) {
+        echo 'Data Added';
+
+        header('location: student.php');
+    } else {
+        echo 'Data Not Added';
+    }
+}
+
+if (isset($_REQUEST['del'])) {
+    $id = $_REQUEST['del'];
+    $delete = "DELETE FROM `students` WHERE id = $id";
+    $query = mysqli_query($conn, $delete);
+
+    if ($query) {
+        header('location: student.php');
+    } else {
+        echo 'Data Not Added';
+    }
+}
+
+$get = "SELECT * FROM `students`";
+$result = mysqli_query($conn, $get);
 ?>
 
 <div class="container-fluid mt-4">
@@ -20,23 +62,36 @@ include("./includes/header.php");
                 <tr>
                     <th>Sno.</th>
                     <th>Student Name</th>
-                    <th>Email</th>
-                    <th>Phone</th>
+                    <th>Father Name</th>
+                    <th>Course</th>
                     <th>Gender</th>
                     <th>Dob</th>
+                    <th>Status</th>
                     <th>Action</th>
                 </tr>
             </thead>
             <tbody>
-                <tr>
-                    <td>01</td>
-                    <td>peter</td>
-                    <td>peter@email.com</td>
-                    <td>213e324</td>
-                    <td>Male</td>
-                    <td>31-10-2003</td>
-                    <td>helo</td>
-                </tr>
+                <?php
+                $a = 0;
+                while ($row = mysqli_fetch_assoc($result)) {
+                    $a++;
+                ?>
+                    <tr>
+                        <td><?= $a ?></td>
+                        <td><?= $row['name'] ?> </td>
+                        <td><?= $row['father_name'] ?></td>
+                        <td><?= $row['course'] ?></td>
+                        <td><?= $row['gender'] ?></td>
+                        <td><?= $row['date_of_birth'] ?></td>
+                        <td><?= $row['status'] ?></td>
+                        <td>
+                            <a href="student.php?edit=<?= $row['id'] ?>"><button class="btn bg-primary text-white btn-sm">Edit</button></a>
+                            <a href="student.php?del=<?= $row['id'] ?>"><button class="btn bg-primary text-white btn-sm">Delete</button></a>
+                        </td>
+                    </tr>
+                <?php
+                }
+                ?>
             </tbody>
         </table>
     </div>
@@ -47,55 +102,111 @@ include("./includes/header.php");
     <div class="modal-dialog modal-xl">
         <div class="modal-content">
             <div class="modal-header">
-                <h1 class="modal-title fs-5" id="exampleModalLabel">Add Student</h1>
+                <h1 class="modal-title fs-5 " id="exampleModalLabel">Add Student</h1>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
                 <div class="container-fluid">
-                    <div class="row">
-                        <div class="col-xl-3 col-lg-4 col-md-4 col-sm-6 ">
-                            <div class="mb-3">
-                                <label for="studentName" class="formlabel mt-1  text-primary">Name</label>
-                                <input type="text" class="form-control mt-1" id="studentName" name="student_name">
+                    <div class="container">
+
+                        <form action="#" method="post" class="form">
+
+                            <div class="row">
+                                <div class="col-md-4 col-12">
+                                    <label for="name" class="form-label text-primary">Name</label>
+                                    <input type="text" placeholder="Enter Name" required name="name" id="name" class="form-control">
+                                </div>
+                                <div class="col-md-4 col-12 mt-md-0 mt-3">
+                                    <label for="roll_no" class="form-label text-primary">Roll No</label>
+                                    <input type="number" min="0" placeholder="Enter Roll No" required name="roll_no" id="roll_no" class="form-control">
+                                </div>
+                                <div class="col-md-4 col-12">
+                                    <label for="father_name" class="form-label text-primary">Father Name</label>
+                                    <input type="text" placeholder="Enter Father Name" required name="father_name" id="father_name" class="form-control">
+                                </div>
                             </div>
-                        </div>
-                        <div class="col-xl-3 col-lg-4 col-md-4 col-sm-6 ">
-                            <div class="mb-3">
-                                <label for="phoneNo" class="formlabel mt-1  text-primary">Father Name</label>
-                                <input type="text" class="form-control mt-1">
+
+                            <div class="row mt-3 ">
+                                <div class="col-md-4 col-12 mt-md-0 mt-3">
+                                    <label for="mother_name" class="form-label text-primary">Mother Name</label>
+                                    <input type="text" placeholder="Enter Mother Name" required name="mother_name" id="mother_name" class="form-control">
+                                </div>
+                                <div class="col-md-4 col-12">
+                                    <label for="contact_no" class="form-label text-primary">Contact No</label>
+                                    <input type="number" min="0" placeholder="Enter Contact No" required name="contact_no" id="contact_no" class="form-control">
+                                </div>
+                                <div class="col-md-4 col-12 mt-md-0 mt-3">
+                                    <label for="date_of_birth" class="form-label text-primary">Date Of Birth</label>
+                                    <input type="date" name="date_of_birth" id="date_of_birth" required class="form-control">
+                                </div>
                             </div>
-                        </div>
-                        <div class="col-xl-3 col-lg-4 col-md-4 col-sm-6 ">
-                            <div class="mb-3">
-                                <label for="phoneNo" class="formlabel mt-1  text-primary">Mother Name</label>
-                                <input type="email" class="form-control mt-1">
+
+
+                            <div class="row mt-3 ">
+                                <div class="col-md-4 col-12">
+                                    <label for="parent_no" class="form-label text-primary">Parent No</label>
+                                    <input type="number" min="0" required placeholder="Enter Parent No" name="parent_no" id="parent_no" class="form-control">
+                                </div>
+                                <div class="col-md-4 col-12 mt-md-0 mt-3">
+                                    <label for="course" class="form-label text-primary">Course</label>
+                                    <select class="form-select" required name="course" id="course" aria-label="Default select example">
+                                        <option selected>Select Course</option>
+                                        <option value="1">One</option>
+                                        <option value="2">Two</option>
+                                        <option value="3">Three</option>
+                                    </select>
+                                </div>
+                                <div class="col-md-4 col-12">
+                                    <label for="gender" class="form-label text-primary">Gender</label>
+                                    <select class="form-select" name="gender" required id="gender" aria-label="Default select example">
+                                        <option selected>Select Gender</option>
+                                        <option value="Male">Male</option>
+                                        <option value="Female">Female</option>
+                                        <option value="others">others</option>
+                                    </select>
+                                </div>
                             </div>
-                        </div>
-                        <div class="col-xl-3 col-lg-4 col-md-4 col-sm-6 ">
-                            <div class="mb-3">
-                                <label for="phoneNo" class="formlabel mt-1  text-primary">Dob</label>
-                                <input type="text" class="form-control mt-1">
+
+                            <div class="row mt-3 ">
+
+                                <div class="col-md-6 col-12 mt-md-0 mt-3">
+                                    <label for="blood_group" class="form-label text-primary">Blood Group</label>
+                                    <select class="form-select" required name="blood_group" id="blood_group" aria-label="Default select example">
+                                        <option selected>Select Blood Group</option>
+                                        <option value="A+">A+</option>
+                                        <option value="A-">A-</option>
+                                        <option value="B+">B+</option>
+                                        <option value="B-">B-</option>
+                                        <option value="O+">O+</option>
+                                        <option value="O-">O-</option>
+                                        <option value="AB+">AB+</option>
+                                        <option value="AB-">AB-</option>
+                                    </select>
+                                </div>
+                                <div class="col-md-6 col-12 mt-md-0 mt-3">
+                                    <label for="status" class="form-label text-primary">Status</label>
+                                    <select class="form-select" required name="status" id="status" aria-label="Default select example">
+                                        <option selected>Select Status</option>
+                                        <option value="Active">Active</option>
+                                        <option value="InActive">InActive</option>
+                                    </select>
+                                </div>
+                                <div class="row mt-3 pe-0">
+                                    <div class="col-12 pe-0">
+                                        <label for="address" class="form-label text-primary">Address</label>
+                                        <textarea placeholder="Enter Your Address" required name="address" id="address" rows="3" class="form-control"></textarea>
+                                    </div>
+                                </div>
+
                             </div>
-                        </div>
-                        <div class="col-xl-3 col-lg-4 col-md-4 col-sm-6 ">
-                            <div class="mb-3">
-                                <label for="email" class="formlabel mt-1  text-primary">Email</label>
-                                <input type="email" class="form-control mt-1">
-                            </div>
-                        </div>
-                        <div class="col-xl-3 col-lg-4 col-md-4 col-sm-6 ">
-                            <div class="mb-3">
-                                <label for="phoneNo" class="formlabel mt-1  text-primary">Phone No</label>
-                                <input type="text" class="form-control mt-1">
-                            </div>
-                        </div>
 
                     </div>
                 </div>
             </div>
             <div class="modal-footer">
-                <button class="btn bg-primary text-white px-3 py-2 fw-semibold">Save</button>
+                <button type="submit" name="save" class="btn bg-primary text-white px-3 py-2 fw-semibold">Save</button>
             </div>
+            </form>
         </div>
     </div>
 </div>
