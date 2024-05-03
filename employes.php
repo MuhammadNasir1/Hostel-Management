62002ff7d728303247960008d6e18f3c7ca552d6
M-Arham Waheed <info.epgae.org@gmail.com>
2024-05-03 09:56:07 +0500

    A3

---
+++ employes.php
@@ -0,0 +1,231 @@
+<?php
+include("./includes/dbconn.php");
+if (@!$_SESSION['login']) {
+    header("Location: login.php");
+}
+
+$title = "Rooms";
+include("./includes/header.php");
+
+
+if (isset($_POST['submit'])) {
+    $id = $_POST['updateID'];
+    $email_id = $_POST['email_id'];
+    $emp_password = md5($_POST['emp_password']);
+    $block_id = $_POST['block_id'];
+    $emp_type = $_POST['emp_type'];
+    $emp_name = $_POST['emp_name'];
+    $emp_gender = $_POST['emp_gender'];
+    $emp_dob = $_POST['emp_dob'];
+    $emp_doj = $_POST['emp_doj'];
+
+    if (empty($id)) {
+        $insert = "INSERT INTO `employes`(`emp_email`, `block_id`, `emp_type`, `emp_name`, `gender`, `date_of_birth`, `date_of_join`) VALUES ('$email_id','$block_id','$emp_type','$emp_name','$emp_gender','$emp_dob','$emp_doj')";
+
+        $user = "INSERT INTO `users`(`user_name`, `email`, `password`, `role`) VALUES ('$emp_name','$email_id','$emp_password','employe')";
+
+        $query = mysqli_query($conn, $user);
+        $query = mysqli_query($conn, $insert);
+        if ($insert) {
+            header('location: employes.php');
+        }
+        if ($user) {
+            header('location: employes.php');
+        }
+    } else {
+        $update = "UPDATE `employes` SET `block_id`='$block_id',`emp_type`='$emp_type',`emp_name`='$emp_name',`gender`='$emp_gender',`date_of_birth`='$emp_dob',`date_of_join`='$emp_doj' WHERE id = $id";
+        $updateQuery = mysqli_query($conn, $update);
+        if ($updateQuery) {
+            header('location: employes.php');
+        }
+    }
+}
+
+
+$get_data = "SELECT * FROM `employes`";
+$result = mysqli_query($conn, $get_data);
+
+$emp_data = "SELECT * FROM `blocks`";
+$res = mysqli_query($conn, $emp_data);
+
+$user_data = "SELECT * FROM `users`";
+$re = mysqli_query($conn, $user_data);
+$fetch = mysqli_fetch_assoc($re);
+
+
+if (isset($_REQUEST['del'])) {
+    $id = $_REQUEST['del'];
+    $delete = "DELETE FROM `employes` WHERE id = $id";
+    $query = mysqli_query($conn, $delete);
+
+    if ($query) {
+        header('location: employes.php');
+    } else {
+        echo 'Data Not Added';
+    }
+}
+
+if (isset($_REQUEST['edit'])) {
+?>
+    <script>
+        $(document).ready(function() {
+            $('#exampleModal').modal('show');
+        });
+    </script>
+<?php
+    $id = $_REQUEST['edit'];
+    $sql = "SELECT * FROM `employes` WHERE id = $id";
+    $resu = mysqli_query($conn, $sql);
+    $updateData  = mysqli_fetch_assoc($resu);
+}
+
+
+
+
+?>
+
+<div class="container-fluid mt-4">
+    <h4 class="text-primary fw-semibold">All Employes</h4>
+    <div class=" shadow p-4 rounded-1">
+        <div class="d-flex justify-content-between  mb-5">
+            <h5 class="text-primary ">Employes List</h5>
+            <button class="btn bg-primary text-white px-3 py-2 fw-semibold" data-bs-toggle="modal" data-bs-target="#exampleModal">Add Employe</button>
+        </div>
+        <table class="table table-responsive" id="myTable">
+            <thead>
+                <tr>
+                    <th>Sno.</th>
+                    <th>Employe Name</th>
+                    <th>Email</th>
+                    <th>Emaploye Type</th>
+                    <th>DOJ</th>
+                    <th>Action</th>
+
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                $a = 0;
+                while ($row = mysqli_fetch_assoc($result)) {
+                    $a++;
+                ?>
+                    <tr>
+                        <td><?= $a ?></td>
+                        <td><?= $row['emp_name'] ?></td>
+                        <td><?= $row['emp_email'] ?></td>
+                        <td><?= $row['emp_type'] ?></td>
+                        <td><?= $row['date_of_join'] ?></td>
+                        <td>
+                            <a href="employes.php?edit=<?= $row['id'] ?>"><button class="btn bg-primary text-white btn-sm">Edit</button></a>
+                            <a href="employes.php?del=<?= $row['id'] ?>"><button class="btn bg-primary text-white btn-sm">Delete</button></a>
+                        </td>
+
+                    </tr>
+                <?php
+                }
+                ?>
+            </tbody>
+        </table>
+    </div>
+</div>
+
+
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-xl">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="exampleModalLabel">Employe Registration</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <div class="container-fluid">
+                    <form action="#" method="post" class="form">
+                        <input type="hidden" name="updateID" value="<?= @$updateData['id'] ?>">
+                        <div class="row">
+                            <?php
+                            if (empty($id)) {
+                            ?>
+                                <div class="col-lg-4">
+                                    <label for="email_id" class="form-label text-primary">Email</label>
+                                    <input type="email" name="email_id" placeholder="Enter Email Address" id="email_id" class="form-control" value="<?= @$updateData['emp_email'] ?>">
+                                </div>
+                                <div class="col-lg-4 mt-lg-0 mt-3">
+                                    <label for="emp_password" class="form-label text-primary">Login Password</label>
+                                    <input type="password" name="emp_password" placeholder="Enter Login Password" id="emp_password" class="form-control">
+                                </div>
+
+                            <?php
+
+                            }
+                            ?>
+                            <div class="<?php if (empty($id)) {
+                                        ?>
+                                col-lg-4
+                                <?php
+
+
+                                        } ?>col-12 mt-lg-0 mt-3">
+                                <label for="block_id" class="form-label text-primary">Block</label>
+                                <select name="block_id" id="block_id" class="form-select">
+                                    <?php
+                                    while ($row = mysqli_fetch_assoc($res)) {
+                                    ?>
+                                        <option value="<?= $row['block_name'] ?>"><?= $row['block_name'] ?></option>
+                                    <?php
+                                    }
+                                    ?>
+                                </select>
+                            </div>
+                        </div>
+
+                        <div class="row mt-3">
+
+                            <div class="col-lg-4">
+                                <label for="emp_type" class="form-label text-primary">Employe Type</label>
+                                <input type="text" name="emp_type" placeholder="Enter Employe Type" id="emp_type" class="form-control" value="<?= @$updateData['emp_type'] ?>">
+                            </div>
+                            <div class="col-lg-4 mt-lg-0 mt-3">
+                                <label for="emp_name" class="form-label text-primary">Employe Name</label>
+                                <input type="text" name="emp_name" placeholder="Enter Employe Name" id="emp_name" class="form-control" value="<?= @$updateData['emp_name'] ?>">
+                            </div>
+
+                            <div class="col-lg-4 mt-lg-0 mt-3">
+                                <label for="emp_gender" class="form-label text-primary">Gender</label>
+                                <select name="emp_gender" id="emp_gender" class="form-select">
+
+                                    <option <?= (@$updateData['gender'] == 'Male') ? 'selected' : ''; ?>>Male</option>
+                                    <option <?= (@$updateData['gender'] == 'Female') ? 'selected' : ''; ?>>Female</option>
+                                    <option <?= (@$updateData['gender'] == 'Others') ? 'selected' : ''; ?>>Others</option>
+
+                                </select>
+                            </div>
+                        </div>
+
+                        <div class="row mt-3">
+                            <div class="col-lg-6">
+                                <label for="emp_dob" class="form-label text-primary">Date Of Birth</label>
+                                <input type="date" name="emp_dob" id="emp_dob" class="form-control" value="<?= @$updateData['date_of_birth'] ?>">
+                            </div>
+                            <div class="col-lg-6 mt-lg-0 mt-3">
+                                <label for="emp_doj" class="form-label text-primary">Date Of Joining</label>
+                                <input type="date" name="emp_doj" id="emp_doj" class="form-control" value="<?= @$updateData['date_of_join'] ?>">
+                            </div>
+
+                        </div>
+
+
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="submit" name="submit" class="btn bg-primary text-white px-3 py-2 fw-semibold">Save</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php
+include("./includes/footer.php")
+?>
---
+++ room_registration.php
@@ -0,0 +1,208 @@
+<?php
+include("./includes/dbconn.php");
+if (@!$_SESSION['login']) {
+    header("Location: login.php");
+}
+
+$title = "Rooms";
+include("./includes/header.php");
+
+
+if (isset($_POST['submit'])) {
+    $id = $_POST['updateID'];
+    $student_id = $_POST['student_id'];
+    $room_id = $_POST['room_id'];
+    $join_date = $_POST['join_date'];
+    $end_date = $_POST['end_date'];
+    $food_type = $_POST['food_type'];
+    $beverage_type = $_POST['beverage_type'];
+    $status = $_POST['status'];
+
+
+    if (empty($id)) {
+        $insert = "INSERT INTO `room_registration`(`student_id`, `room_id`, `join_date`, `end_date`, `food_type`, `beverage_type`, `status`) VALUES ('0','0','$join_date','$end_date','$food_type','$beverage_type','$status')";
+        $query = mysqli_query($conn, $insert);
+        if ($insert) {
+            header('location: room_registration.php');
+        }
+    } else {
+        $update = "UPDATE `room_registration` SET `student_id`='0',`room_id`='0',`join_date`='$join_date',`end_date`='$end_date',`food_type`='$food_type',`beverage_type`='$beverage_type',`status`='$status' WHERE id = $id";
+        $updateQuery = mysqli_query($conn, $update);
+        if ($updateQuery) {
+            header('location: room_registration.php');
+        }
+    }
+}
+
+
+$get_data = "SELECT * FROM `room_registration`";
+$result = mysqli_query($conn, $get_data);
+
+$student_data = "SELECT * FROM `students`";
+$res = mysqli_query($conn, $student_data);
+
+$rooms_data = "SELECT * FROM `rooms`";
+$re = mysqli_query($conn, $rooms_data);
+
+
+if (isset($_REQUEST['del'])) {
+    $id = $_REQUEST['del'];
+    $delete = "DELETE FROM `room_registration` WHERE id = $id";
+    $query = mysqli_query($conn, $delete);
+
+    if ($query) {
+        header('location: room_registration.php');
+    } else {
+        echo 'Data Not Added';
+    }
+}
+
+if (isset($_REQUEST['edit'])) {
+?>
+    <script>
+        $(document).ready(function() {
+            $('#exampleModal').modal('show');
+        });
+    </script>
+<?php
+    $id = $_REQUEST['edit'];
+    $sql = "SELECT * FROM `room_registration` WHERE id = $id";
+    $resu = mysqli_query($conn, $sql);
+    $updateData  = mysqli_fetch_assoc($resu);
+}
+
+
+?>
+
+<div class="container-fluid mt-4">
+    <h4 class="text-primary fw-semibold">All Rooms</h4>
+    <div class=" shadow p-4 rounded-1">
+        <div class="d-flex justify-content-between  mb-5">
+            <h5 class="text-primary ">Rooms List</h5>
+            <button class="btn bg-primary text-white px-3 py-2 fw-semibold" data-bs-toggle="modal" data-bs-target="#exampleModal">Register Room</button>
+        </div>
+        <table class="table table-responsive" id="myTable">
+            <thead>
+                <tr>
+                    <th>Sno.</th>
+                    <th>Join Date</th>
+                    <th>End Date</th>
+                    <th>Food Type</th>
+                    <th>Beverage Type</th>
+                    <th>Action</th>
+
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                $a = 0;
+                while ($row = mysqli_fetch_assoc($result)) {
+                    $a++;
+                ?>
+                    <tr>
+                        <td><?= $a ?></td>
+                        <td><?= $row['join_date'] ?></td>
+                        <td><?= $row['end_date'] ?></td>
+                        <td><?= $row['food_type'] ?></td>
+                        <td><?= $row['beverage_type'] ?></td>
+                        <td>
+                            <a href="room_registration.php?edit=<?= $row['id'] ?>"><button class="btn bg-primary text-white btn-sm">Edit</button></a>
+                            <a href="room_registration.php?del=<?= $row['id'] ?>"><button class="btn bg-primary text-white btn-sm">Delete</button></a>
+                        </td>
+
+                    </tr>
+                <?php
+                }
+                ?>
+            </tbody>
+        </table>
+    </div>
+</div>
+
+
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-xl">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="exampleModalLabel">Room Registration</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <div class="container-fluid">
+                    <form action="#" method="post" class="form">
+                        <input type="hidden" name="updateID" value="<?= @$updateData['id'] ?>">
+                        <div class="row">
+                            <div class="col-lg-6">
+                                <label for="student_id" class="form-label">Student</label>
+                                <select name="student_id" id="student_id" class="form-select">
+                                    <?php
+                                    while ($row = mysqli_fetch_assoc($res)) {
+                                    ?>
+                                        <option <?= (@$updateData['student_id'] == $row['name']) ? 'selected' : ''; ?> value="<?= $row['name'] ?>"><?= $row['name'] ?></option>
+                                    <?php
+                                    }
+                                    ?>
+                                </select>
+                            </div>
+                            <div class="col-lg-6 mt-lg-0 mt-3">
+                                <label for="room_id" class="form-label">Room</label>
+                                <select name="room_id" id="room_id" class="form-select">
+                                    <?php
+                                    while ($row = mysqli_fetch_assoc($re)) {
+                                    ?>
+                                        <option <?= (@$updateData['room_id'] == $row['room_no']) ? 'selected' : ''; ?> value="<?= $row['room_no'] ?>"><?= $row['room_no'] ?></option>
+                                    <?php
+                                    }
+                                    ?>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="row mt-3">
+                            <div class="col-lg-6">
+                                <label for="join_date" class="form-label">Join Date</label>
+                                <input type="date" name="join_date" id="join_date" class="form-control" value="<?= @$updateData['join_date'] ?>">
+                            </div>
+                            <div class="col-lg-6 mt-lg-0 mt-3">
+                                <label for="end_date" class="form-label">End Date</label>
+                                <input type="date" name="end_date" id="end_date" class="form-control" value="<?= @$updateData['end_date'] ?>">
+                            </div>
+                        </div>
+                        <div class="row mt-3">
+                            <div class="col-lg-6">
+                                <label for="food_type" class="form-label">Food Type</label>
+                                <select name="food_type" id="food_type" class="form-select">
+                                    <option <?= (@$updateData['food_type'] == 'With Food') ? 'selected' : ''; ?> value="With Food">With Food</option>
+                                    <option <?= (@$updateData['food_type'] == 'Without Food') ? 'selected' : ''; ?> value="Without Food">Without Food</option>
+                                </select>
+                            </div>
+                            <div class="col-lg-6 mt-lg-0 mt-3">
+                                <label for="beverage_type" class="form-label">Beverage Type</label>
+                                <input type="text" name="beverage_type" id="beverage_type" class="form-control" value="<?= @$updateData['beverage_type'] ?>">
+                            </div>
+                        </div>
+                        <div class="row mt-3">
+                            <div class="col">
+                                <label for="status" class="form-label">Status</label>
+                                <select name="status" id="status" class="form-select">
+                                    <option <?= (@$updateData['status'] == 'Active') ? 'selected' : ''; ?> value="Active">Active</option>
+                                    <option <?= (@$updateData['status'] == 'InActive') ? 'selected' : ''; ?> value="InActive">InActive</option>
+                                </select>
+                            </div>
+                        </div>
+
+
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="submit" name="submit" class="btn bg-primary text-white px-3 py-2 fw-semibold">Save</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php
+include("./includes/footer.php")
+?>
