90966c0f836c933a66c6cca44c4ca7fe12dc6c4d
M-Arham Waheed <info.epgae.org@gmail.com>
2024-05-11 12:11:24 +0500

    dashboard

--- includes/header.php
+++ includes/header.php
@@ -8,6 +8,7 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   <link rel="stylesheet" href="./css/style.css">
   <link rel="stylesheet" href="./DataTables/datatables.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
 </head>
@@ -17,24 +18,28 @@
   <div class="main-container d-flex">
     <div class="sidebar " id="side_nav">
       <div class="header-box px-2 pt-3 pb-4 d-flex justify-content-between">
-        <h1 class="fs-4"><span class="text-white">HMS</span></h1>
+        <h1 class="fs-4"><span class="text-white ps-3">HMS</span></h1>
         <button class="btn d-md-none d-block close-btn px-1 py-0 text-white"><i class="fal fa-stream"></i></button>
       </div>
 
       <ul class="list-unstyled px-2">
-        <li class="active"><a href="#" class="text-decoration-none px-3 py-2 d-block"><i class="fal fa-home"></i> Dashboard</a></li>
-        <li class=""><a href="#" class="text-decoration-none px-3 py-2 d-block"><i class="fal fa-home"></i> Fees</a></li>
-        <li class=""><a href="./student.php" class="text-decoration-none px-3 py-2 d-block"><i class="fal fa-home"></i> Students</a></li>
-        <li class=""><a href="./course.php" class="text-decoration-none px-3 py-2 d-block"><i class="fal fa-envelope-open-text"></i> Courses</a></li>
-        <li class=""><a href="./rooms.php" class="text-decoration-none px-3 py-2 d-block"><i class="fal fa-users"></i>
+        <li class="active"><a href="index.php" class="text-decoration-none px-3 py-2 d-block">Dashboard</a></li>
+        <li class=""><a href="#" class="text-decoration-none px-3 py-2 d-block"> Fees</a></li>
+        <li class=""><a href="./student.php" class="text-decoration-none px-3 py-2 d-block"> Students</a></li>
+        <li class=""><a href="./course.php" class="text-decoration-none px-3 py-2 d-block"> Courses</a></li>
+        <li class=""><a href="./rooms.php" class="text-decoration-none px-3 py-2 d-block">
             Rooms</a></li>
+        <li class=""><a href="./employes.php" class="text-decoration-none px-3 py-2 d-block">
+            Employes</a></li>
+        <li class=""><a href="./blocks.php" class="text-decoration-none px-3 py-2 d-block">
+            Blocks</a></li>
       </ul>
       <hr class="h-color mx-2">
 
       <ul class="list-unstyled px-2">
-        <li class=""><a href="#" class="text-decoration-none px-3 py-2 d-block"><i class="fal fa-bars"></i>
+        <li class=""><a href="#" class="text-decoration-none px-3 py-2 d-block">
             Settings</a></li>
-        <li class=""><a href="#" class="text-decoration-none px-3 py-2 d-block"><i class="fal fa-bell"></i>
+        <li class=""><a href="#" class="text-decoration-none px-3 py-2 d-block">
             Notifications</a></li>
 
       </ul>
--- index.php
+++ index.php
@@ -4,10 +4,51 @@ if (@!$_SESSION['login']) {
     header("Location: login.php");
 }
 
-$title = "HOME";
+$title = "Dashboard";
 include("./includes/header.php");
 ?>
 
+<div class="container dashboard-heading">
+    <h1>Dashboard</h1>
+</div>
+<div class="container d-flex justify-content-center gap-5 dashboard flex-wrap">
+    <div class="box">
+        <div class="d-flex align-items-center flex-column ps-2">
+            <div class="card-heading">Total Students</div>
+            <div class="number">03</div>
+        </div>
+        <div class="mx-auto">
+            <i class="fa-solid fa-graduation-cap"></i>
+        </div>
+    </div>
+    <div class="box">
+        <div class="d-flex align-items-center flex-column ps-2">
+            <div class="card-heading">Total Nurses</div>
+            <div class="number">03</div>
+        </div>
+        <div class="mx-auto">
+            <i class="fa-solid fa-bed-pulse"></i>
+        </div>
+    </div>
+    <div class="box">
+        <div class="d-flex align-items-center flex-column ps-2">
+            <div class="card-heading">Total Employes</div>
+            <div class="number">03</div>
+        </div>
+        <div class="mx-auto">
+            <i class="fa-solid fa-users-line"></i>
+        </div>
+    </div>
+    <div class="box">
+        <div class="d-flex align-items-center flex-column ps-2">
+            <div class="card-heading">Total Revenue</div>
+            <div class="number">03</div>
+        </div>
+        <div class="mx-auto">
+            <i class="fa-solid fa-sack-dollar"></i>
+        </div>
+    </div>
+</div>
 
 
 <?php
