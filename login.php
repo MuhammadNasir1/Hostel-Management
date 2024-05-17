0055bff626612545453a822e973be22157288880
M-Arham Waheed <info.epgae.org@gmail.com>
2024-05-17 15:03:20 +0500

    loginPageUpdate

--- login.php
+++ login.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Logins</title>
     <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
 </head>
 
 <body>
@@ -27,16 +28,28 @@
         }
     }
     ?>
-    <form action="#" class="login" method="POST">
-        <h2>Welcome, User!</h2>
-        <p>Please log in</p>
-        <input type="email" placeholder="Email" name="email" />
-        <input type="password" placeholder="Password" name="password" />
-        <?php
-        echo @$error;
-        ?>
-        <input type="submit" value="Log In" />
+    <div class="container login-page d-flex justify-content-center align-items-center">
 
+        <div class="form-container d-flex">
+            <div class="part-1 text-white d-flex justify-content-center align-items-center flex-column">
+                <h3>Hello</h3>
+                <h1 class="fw-bold">Welcome!</h1>
+            </div>
+            <div class="part-2 d-flex justify-content-center align-items-center flex-column ">
+                <form action="#" method="POST" class="form d-flex flex-column justify-content-center gap-2">
+                    <h2 class="text-center fw-bold">LOGIN</h2>
+                    <div class="mt-2"><input type="email" placeholder="Email" name="email" class="form-control" /></div>
+                    <div class="mt-2"> <input type="password" placeholder="Password" name="password" class="form-control" /></div>
+                    <?php
+                    echo @$error;
+                    ?>
+                    <input type="submit" value="Log In" class="mt-2 py-2" />
+            </div>
+        </div>
+
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
 </html>
