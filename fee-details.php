54adb49b6594610cef5413cf199885aac60e1cd8
MuhammadNasir1 <nmian7080@gmail.com>
2024-05-27 00:22:48 +0500

    fix bugs

--- fee-details.php
+++ fee-details.php
@@ -6,21 +6,16 @@ if (@!$_SESSION['login']) {
 
 $title = "Fee Details";
 
-
-include("./includes/header.php");
-if (isset($_REQUEST['edit'])) {
-?>
-    <script>
-        $(document).ready(function() {
-            $('#exampleModal').modal('show');
-        });
-    </script>
-<?php
-    $id = $_REQUEST['edit'];
-    $sql = "SELECT * FROM `rooms` WHERE id = $id";
-    $resu = mysqli_query($conn, $sql);
-    $updateData  = mysqli_fetch_assoc($resu);
+if (isset($_POST['submit'])) {
+    $updateId = $_POST['updateId'];
+    $status = $_POST['fee_status'];
+    $result = mysqli_query($conn, "UPDATE room_registration SET fee_status = '$status' WHERE id = $updateId");
+    if ($result) {
+        header('location: ./fee-details.php');
+    }
 }
+include("./includes/header.php");
+
 
 ?>
 
@@ -86,7 +81,7 @@ if (isset($_REQUEST['edit'])) {
                         <td><button class="btn <?= (isset($data['fee_status']) && $data['fee_status'] == "pending") ? "bg-danger" : "bg-success" ?>
  text-white px-3 py-2 fw-semibold">
                                 <span></span> <?= $data['fee_status'] ?></button></< /td>
-                        <td> <button class="btn bg-primary text-white px-3 py-2 fw-semibold">
+                        <td> <button userId="<?= $data['id'] ?>" class="btn bg-primary text-white px-3 py-2 fw-semibold statusChangeBtn" data-bs-toggle="modal" data-bs-target="#exampleModal">
                                 <span></span> Change Status</button></td>
                     </tr>
                 <?php
@@ -98,9 +93,47 @@ if (isset($_REQUEST['edit'])) {
 </div>
 
 
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header bg-primary text-white d-flex justify-content-between align-items-center">
+                <h1 class="modal-title fs-5 " id="exampleModalLabel">Fee Status</h1>
+                <span type="button" class="pt-1" data-bs-dismiss="modal" aria-label="Close"><span class="text-white"><i class="fa-solid fa-xmark fs-4"></i></span></span>
+            </div>
+            <div class="modal-body">
+                <div class="container-fluid">
+                    <form action="#" method="post" class="form">
+                        <input type="hidden" id="statusUpdateId" name="updateId" value="1">
+                        <div class="col-12">
+                            <label for="fee_status" class="form-label">Fee Status</label>
+                            <select name="fee_status" id="fee_status" class="form-select">
+                                <option selected disabled>Select Status</option>
+                                <option value="paid">Paid</option>
+                                <option value="pending">Pending</option>
+                            </select>
+                        </div>
+
+
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="submit" name="submit" class="btn bg-primary text-white px-3 py-2 fw-semibold">Update</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
 
 
 
 <?php
 include("./includes/footer.php")
 ?>
+
+<script>
+    $('.statusChangeBtn').click(function() {
+        var statusId = $(this).attr('userId');
+        $('#statusUpdateId').val(statusId);
+    });
+</script>
